8bf2d44519f90591fd8adcf7ed26b6018c2efa3f
DESKTOP-D3IQGRD\李姳諼 <djuszx@gmail.com>
2018-06-21 13:55:17 +0800

    no message

---
+++ WpfApplication4/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        // 進入程式時
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            string[] lines = { };
+            // 如果有檔案就讀取
+            try
+            {
+                lines = System.IO.File.ReadAllLines(@"D:\accountingApp\data.txt");
+            }
+            // 如果沒檔案就新增
+            catch
+            {
+                List<string> standard = new List<string>();
+                System.IO.File.WriteAllLines(@"D:\accountingApp\data.txt", standard);
+            }
+
+            // 讀取每行
+            foreach (string line in lines)
+            {
+                // 用 | 隔開
+                string[] parts = line.Split('|');
+
+                // 建立 category
+                category categorys = new category();
+
+                // 讀取category的資料
+                categorys.categoryDate.Text = parts[0];
+                categorys.categoryName.Text = parts[1];
+                categorys.categoryPrice.Text = parts[2];
+
+                // 放到清單中
+                categoryList.Children.Add(categorys);
+            }
+        }
+        // 按下+鍵時
+        private void addBtn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // 產生項目
+            category categorys = new category();
+
+            //放到清單中
+            categoryList.Children.Add(categorys);
+        }
+
+        // 關閉程式時
+        private void windowClosed(object sender, EventArgs e)
+        {
+            List<string> Texts = new List<string>();
+
+            foreach (category categorys in categoryList.Children)
+            {
+
+                string data = "";
+
+                // 每一種資料以"|"分隔加入Texts字串
+                data += categorys.categoryDate.Text + "|" + categorys.categoryName.Text + "|" + categorys.categoryPrice.Text;
+
+                // 存入Texts的陣列
+                Texts.Add(data);
+            }
+            //儲存到D:\
+            System.IO.File.WriteAllLines(@"D:\accountingApp\data.txt", Texts);
+        }
+
+        // 使用者切換欄位時 自動計算TOTAL
+        private void Window_GotFocus(object sender, RoutedEventArgs e)
+        {
+            int totalprice = 0;
+
+
+            foreach (category categorys in categoryList.Children)
+            {
+                totalprice += categorys.setPriceValue;
+            }
+
+            //顯示價格
+            total.Text = totalprice.ToString();
+
+        }
+    }
+}
---
+++ WpfApplication4/category.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication4
+{
+    /// <summary>
+    /// Interaction logic for category.xaml
+    /// </summary>
+    public partial class category : UserControl
+    {
+        public category()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public int setPriceValue
+        {
+            // 避免輸入非數字的值
+            get
+            {
+
+                try
+                {
+                    return int.Parse(categoryPrice.Text);
+                }
+
+                catch
+                {
+                    if (categoryPrice.Text != "")
+                    {
+                        MessageBox.Show("只能輸入數字");
+                    }
+                    return 0;
+                }
+            }
+            set
+            {
+                categoryPrice.Text = value.ToString();
+            }
+        }
+
+        // 使用者點選日期時
+        private void categoryDate_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // 清空預設日期
+            categoryDate.Text = "";
+        }
+
+        // 使用者點選價格時
+        private void categoryPrice_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // 清空預設價格
+            categoryPrice.Text = "";
+        }
+
+        // 使用者點選項目名稱時
+        private void categoryName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // 清空預設名稱
+            categoryName.Text = "";
+        }
+
+        // 使用者沒有點選項目名稱時
+        private void categoryName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // 如果使用者前一次沒有輸入資料
+            if (categoryName.Text == "")
+            {
+                // 設定回預設內容
+                categoryName.Text = "請輸入項目名稱";
+            }
+
+        }
+        // 使用者沒有點選價格欄位時
+        private void categoryPrice_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // 如果使用者前一次沒有輸入資料
+            if (categoryPrice.Text == "")
+            {
+                // 設定回預設內容
+                categoryPrice.Text = "0";
+            }
+        }
+        // 使用者沒有點選日期欄位時
+        private void categoryDate_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // 如果使用者前一次沒有輸入資料
+            if (categoryDate.Text == "")
+            {
+                // 設定為當前日期
+                categoryDate.Text = DateTime.Now.ToString("M / dd");
+            }
+        }
+
+        private void categoryDate_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 預設日期為當日
+            categoryDate.Text = DateTime.Now.ToString("M / dd");
+        }
+    }
+}
